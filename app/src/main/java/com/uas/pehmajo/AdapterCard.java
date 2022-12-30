dc7ef7527bcf7668adf1eb86ae447e6706b4b9f0
syifawhn <syifawahyuni@mhs.mdp.ac.id>
2022-12-30 19:44:21 +0700

    p

---
+++ app/src/main/java/com/uas/pehmajo/AdapterCard.java
@@ -0,0 +1,12 @@
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.uas.pehmajo.MainActivity;
+import com.uas.pehmajo.R;
+import com.bu
